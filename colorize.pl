8c5ba687b303e1cf8560c8a0bef135c999843f1c
oxnz <yunxinyi@gmail.com>
2014-08-12 11:02:10 +0800

    refine and update the test file

--- colorize.pl
+++ colorize.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 
 {
-	my %color = (
+	my %colors = (
 		'black'	=> 30,
 		'red'	=> 31,
 		'green'	=> 32,
@@ -16,18 +16,24 @@ use warnings;
 		'default'	=> 39
 	);
 
-	my %subst = (
-		'debug'		=> 'green',
-		'info'	=> 'cyan',
-		'warning|warn'	=> 'yellow',
-		'error|fatal'		=> 'red',
+	my %levels = (
+		'debug|(\[debug\]):?' => 'cyan',
+		'info|(\[info\]):?'	=> 'green',
+		'((warn(ing)?)|(\[warn(ing)?\])):?'	=> 'yellow',
+		'(((fatal )?error)|(\[(fatal )?error\])|fatal):?'	=> 'red',
+	);
+
+	my %keywords = (
 		'\'.*?\''	=> 'blue', # single quote
-		'\".*?\"'	=> 'blue', # double quote
+		'\".*?\"'	=> 'magneta', # double quote
 	);
 
 	sub colorize {
-		while (my ($keywords, $colorkey) = each %subst) {
-			s/($keywords)/\e[$color{$colorkey}m$1\e[m/ig;
+		while (my ($level, $color) = each %levels) {
+			s/(^$level)/\e[$colors{$color}m$1\e[m/i;
+		}
+		foreach my $keyword (%keywords) {
+			s/($keyword)/\e[$colors{$keywords{$keyword}}m$1\e[m/g;
 		}
 	}
 }
