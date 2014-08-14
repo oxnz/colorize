2927727be60535ed361f60653c487d58db1d0fc1
oxnz <yunxinyi@gmail.com>
2014-08-15 01:28:55 +0800

    add more pattern; add pod

--- colorize.pl
+++ colorize.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/env perl
 
+# Author: nz
+# Email: yunxinyi@gmail.com
+# Version: 0.1
+
 use strict;
 use warnings;
 
@@ -52,14 +56,22 @@ use warnings;
 		qr/\".*?\"/	=> {
 			'color'	=> 'magneta', # double quote
 			'font'	=> 'bold'
+		},
+		qr/\(.*?\)/	=> {
+			'color'	=> 'green', # double quote
+			'font'	=> 'normal'
+		},
+		qr/\{.*?\}/ => {
+			'color'	=> 'blue',
+			'font'	=> 'normal'
+		},
+		qr/%\d+/ => {
+			'color' => 'yellow',
+			'font'	=> 'normal'
 		}
 	);
 
 
-	#foreach my $font (keys %fonts) {
-	#	print $fonts{$font}{'color'}, "\n";
-	#}
-	#exit;
 	sub colorize {
 		foreach my $pattern (keys %levels) {
 			last if s/(^$pattern)/\e[$fonts{$levels{$pattern}{'font'}};$colors{$levels{$pattern}{'color'}}m$1\e[m/i;
@@ -70,8 +82,51 @@ use warnings;
 	}
 }
 
-
 while (<>) {
 	&colorize;
 	print;
 }
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+colorize - colorize for specific field(s) in text file(s).
+
+=head1 SYNOPSIS
+
+colorize [-h]
+
+Examples:
+
+colorize /var/log/confluence.log
+
+tail -f /var/log/tomcat.log | colorize
+
+See below for more description of the switches.
+
+=head1 DESCRIPTION
+
+colorize is a shell script used to colorize specific fields of text files.
+
+=head1 OPTIONS
+
+=over 8
+
+=item -h --help print help message and exit
+
+=back
+
+=head1 SEE ALSO
+
+tail
+
+=head1 AUTHOR
+
+Current maintainer: nz L<yunxinyi@gmail.com>
+
+=cut
